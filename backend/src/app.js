ea9b8dbc1c0d285be16522c1ace48cfed73f5bdb
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-07-20 21:37:09 -0300

    Configuracion del Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+//settings
+app.set('port', process.env.PORT || 4000)
+
+//middlewares --> Son funciones que se ejecutan antes de las URLs
+app.use(cors());
+app.use(express.json())
+
+//routes
+app.get('/api/users', (req, res) => {
+    res.send('Users Routes');
+});
+app.get('/api/notes', (req, res) => {
+    res.send('Notes Routes');
+});
 
 module.exports = app;
