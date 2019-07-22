e0927294e0aeeea03494afd3f53a5a0f4f866ca8
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-07-22 14:54:16 -0300

    MongoDB - Queries with mongoose

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -1,13 +1,49 @@
 const notesCtrl = {};
 
-notesCtrl.getNotes = (req,res)=> res.json({message: []})
+const Note = require('../models/Note')
 
-notesCtrl.createNote = (req,res)=> res.json({message: 'Note saved'})
+notesCtrl.getNotes = async (req,res)=> {
+    const notes = await Note.find()
+    res.json(notes)
 
-notesCtrl.updateNote = (req,res)=> res.json({message: 'Note Updated'})
+}
 
-notesCtrl.deleteNote = (req,res)=> res.json({message: 'Note deleted'})
+notesCtrl.createNote = async (req,res)=> {
+    const {title, content, date, author} = req.body;
+    const newNote = new Note({
+        title: title,
+        content: content,
+        author: author
+    })
+    console.log(newNote);
+    await newNote.save()
+    res.json({message: 'Note saved'})
 
-notesCtrl.getNote = (req,res)=> res.json({title: "asdada"})
+}
+notesCtrl.getNote = async (req,res)=> {
+
+    const note = await Note.findById(req.params.id)
+    res.json(note)
+
+}
+
+notesCtrl.deleteNote = async (req,res)=> {
+
+    await Note.findByIdAndDelete(req.params.id)
+    res.json({message: 'Note deleted'})
+}
+
+notesCtrl.updateNote = async (req,res)=> {
+    const {title, content, author} = req.body 
+    await Note.findOneAndUpdate({_id: req.params.id},{
+        title,
+        content,
+        author,
+    });
+    res.json({message: 'Note Updated'})
+
+}
+
+
 
 module.exports = notesCtrl;
--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,10 +1,26 @@
 const userCtrl = {}
 
-userCtrl.getUsers = (req, res) => res.send("GET - Users Routes")
+const User = require('../models/User')
 
-userCtrl.createUser = (req, res) => res.send("GET - Users Routes")
+userCtrl.getUsers = async (req, res) => {
+    const users = await User.find()
+    res.json(users)
 
-userCtrl.deleteUser = (req, res) => res.send("GET - Users Routes")
+}
+
+userCtrl.createUser = async  (req, res) => {
+    const {username} = req.body
+    const newUser = new User({username})
+    await newUser.save()
+    res.json({message: 'User Created'})
+
+}
+
+userCtrl.deleteUser = async (req, res) => {
+    await User.findByIdAndDelete(req.params.id)
+    res.json({message:'User Deleted'})
+
+}
 
 
 module.exports = userCtrl;
